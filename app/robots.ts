e6118c485b5f09d0f87b56635008d292c5349291
SHAFIKUL ISLAM <65854175+shafik4bd@users.noreply.github.com>
2026-06-07 12:50:43 +0600

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/_next/"],
     },
-    sitemap: "https://tv.shajon.dev/sitemap.xml",
+    sitemap: "https://live-tv-bd.vercel.app/sitemap.xml",
   };
 }
