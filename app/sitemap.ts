0e5114116a93037af5894f1a87a5d8f7d0bc2f44
SHAFIKUL ISLAM <65854175+shafik4bd@users.noreply.github.com>
2026-06-07 12:51:10 +0600

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://tv.shajon.dev";
+  const baseUrl = "https://live-tv-bd.vercel.app/";
   
   return [
     {
